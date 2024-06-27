9d4d5889716fd38835dd3aaf5bfee3dc0d5d34ab
stefanygeraldine <stefanygeraldine@gmail.com>
2024-06-27 05:57:01 +0200

    flag texture working

--- src/three/FlagShader.tsx
+++ src/three/FlagShader.tsx
@@ -3,40 +3,50 @@ import * as dat from "dat.gui";
 import { IObjectProps } from "../types.ts";
 import testVertexShader from "../shaders/flag/vertex.glsl";
 import testFragmentShader from "../shaders/flag/fragment.glsl";
-import { forwardRef, useImperativeHandle } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import MyTexture from "../assets/textures/gradients/texture.png";
 
 const FlagShader = forwardRef((props: IObjectProps, ref) => {
   const { scene } = props;
-  const texture = new THREE.TextureLoader();
-  texture.load("flag-french.jpg");
+  const textureLoader = new THREE.TextureLoader();
+  const meshRef = useRef<THREE.Mesh>();
 
-  const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
-  const material = new THREE.RawShaderMaterial({
-    vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader,
-    side: THREE.DoubleSide,
-    uniforms: {
-      uFrequency: { value: new THREE.Vector2(10, 5) },
-      uTime: { value: 0 },
-      uColor: { value: new THREE.Color("orange") },
-      uTexture: { value: texture },
-    },
+  textureLoader.load(MyTexture, (texture) => {
+    const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+    const material = new THREE.RawShaderMaterial({
+      vertexShader: testVertexShader,
+      fragmentShader: testFragmentShader,
+      side: THREE.DoubleSide,
+      uniforms: {
+        uFrequency: { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0 },
+        uColor: { value: new THREE.Color("orange") },
+        uTexture: { value: texture },
+      },
+    });
+
+    const gui = new dat.GUI();
+    const frequencyFolder = gui.addFolder("frequency");
+    frequencyFolder.add(material.uniforms.uFrequency.value, "x", 0, 20, 0.01);
+    frequencyFolder.add(material.uniforms.uFrequency.value, "y", 0, 20, 0.01);
+
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.y = 2 / 3;
+    scene.add(mesh);
+    meshRef.current = mesh;
   });
-
-  const gui = new dat.GUI();
-  const cameraFolder = gui.addFolder("frequency");
-  cameraFolder.add(material.uniforms.uFrequency.value, "x", 0, 20, 0.01);
-  cameraFolder.add(material.uniforms.uFrequency.value, "y", 0, 20, 0.01);
-
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.scale.y = 2 / 3;
-  scene.add(mesh);
 
   useImperativeHandle(ref, () => ({
     updateTime: (time: number) => {
-      material.uniforms.uTime.value = time;
+      if (meshRef.current) {
+        (
+          meshRef.current.material as THREE.ShaderMaterial
+        ).uniforms.uTime.value = time;
+      }
     },
   }));
+
+  return null;
 });
 
 export default FlagShader;
