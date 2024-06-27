f2eeee833b6bb5b753bc3b1985b4e49cabfbdcad
stefanygeraldine <stefanygeraldine@gmail.com>
2024-06-27 05:59:20 +0200

    add bright by elevation

--- src/shaders/flag/vertex.glsl
+++ src/shaders/flag/vertex.glsl
@@ -8,6 +8,7 @@ attribute vec3 position;
 attribute vec2 uv;
 
 varying vec2 vUv;
+varying float vElevation;
 
 
 void main() {
@@ -15,11 +16,18 @@ void main() {
     // Flag waves
     modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
     modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    modelPosition.z += elevation;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
 
     vUv = uv;
+    vElevation = elevation;
 
 }
